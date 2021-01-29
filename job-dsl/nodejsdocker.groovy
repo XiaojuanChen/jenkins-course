b7dec7cfe29d8519266dd351e9db5346ee50b465
XiaojuanChen <bjchenxj@cn.ibm.com>
2021-01-29 18:45:45 +0800

    change repositoryName

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('joanbj2003/docker-example-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
